1ad2993782cd96de98f6205c80d0e844aedfeb3f
NerushKirill <97055285+NerushKirill@users.noreply.github.com>
2022-10-20 21:39:12 +0300

    v.2

--- serv.py
+++ serv.py
@@ -1,14 +1,13 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0', 2222))
-s.listen()
+s.bind(("0.0.0.0", 2222))
+s.listen(10)
 
 while True:
-    conn, adrr = s.accept()
-    while True:
-        data = conn.recv(1024)
-        if not data == 'close':
-            break
-        conn.send(data)
+    conn, addr = s.accept()
+    data = conn.recv(1024)
+    if not data or data == "close":
+        conn.close()   
+    conn.send(data)
     conn.close()
